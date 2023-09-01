d533852a69960d6d7e2cfcc328f87943d2fe7ff6
Taras Iskiv <taras.iskiv@gmail.com>
2023-09-01 23:08:39 +0200

    Added and configured MudBlazor for the client

--- TeamManager.Client/Program.cs
+++ TeamManager.Client/Program.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TeamManager.Client;
 using TeamManager.Client.Logic.Implementation;
 using TeamManager.Client.Logic.Abstraction;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
 
 //Services
 builder.Services.AddScoped<ITeamService, TeamService>();
